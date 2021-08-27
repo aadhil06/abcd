75e56f2fc49984ea050ea0dafefe3a15768f3fa7
aadhil06 <aadhilks007@gmail.com>
2021-08-27 00:02:43 -0700

    sec

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'todo_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': 'todoapp',
         'USER': 'root',
         'PASSWORD': '',
